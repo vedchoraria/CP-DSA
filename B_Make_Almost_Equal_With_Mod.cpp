148e9ed43c658b173b836ec113ecacc2185e4be7
“Ved <vedchoraria19@gmail.com>
2026-02-15 20:02:24 +0530

    15-02-2026

---
+++ B_Make_Almost_Equal_With_Mod.cpp
@@ -0,0 +1,70 @@
+//Author : VED CHORARIA
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Macros ---
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define cin(x) for(auto &i : (x)) cin >> i;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define print(x) cout << (x) << endl;
+#define print_arr(x) for(int &i : (x)) cout << (x) << endl; cout<<endl;
+
+
+
+// --- Constants ---
+const int MOD = 1e9 + 7;
+const ll INF = 1e18; // Changed to ll to match 1e18
+
+// --- General Functions ---
+bool isCo_Prime( int a , int b){
+    return true ? gcd(a,b) == 1 : false;
+}
+
+long long power(long long base, long long exp) {
+    long long res = 1;
+    while (exp > 0) {
+        if (exp & 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp >>= 1;
+    }
+    return res;
+}
+// --- Logic ---
+void solve() {
+    int n;
+    if (!(cin >> n)) return;
+    
+    vector<ll> b(n); 
+    cin(b);
+
+    
+    ll k = 1;
+    for(ll i =0;i<60 ; i++){
+        unordered_map<ll,ll>mpp;
+        k*= 2;
+        for(int j=0;j<n;j++){
+            mpp[b[j]%k]++;
+        }
+
+        if(mpp.size() == 2){
+            print(k);
+            return;
+        }
+
+    }
+
+    // Solve logic here
+    
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
