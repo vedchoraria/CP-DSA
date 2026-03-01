75624e013fb852fd697a6a9d2aa5117daed7f2cd
“Ved <vedchoraria19@gmail.com>
2026-03-01 15:29:06 +0530

    01-03-2026

---
+++ D_Plus_Minus_Permutation.cpp
@@ -0,0 +1,70 @@
+//Author : VED CHORARIA
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Macros ---
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define cin(x) for(auto &i : (x)) cin >> i;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define print(x) cout << (x) << endl;
+#define print_arr(x) for(int &i : (x)) cout << (x) << endl; cout<<endl;
+
+
+
+// --- Constants ---
+const int MOD = 1e9 + 7;
+const ll INF = 1e18; // Changed to ll to match 1e18
+
+// --- General Functions ---
+bool isCo_Prime( int a , int b){
+    return true ? gcd(a,b) == 1 : false;
+}
+
+long long power(long long base, long long exp) {
+    long long res = 1;
+    while (exp > 0) {
+        if (exp & 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp >>= 1;
+    }
+    return res;
+}
+// --- Logic ---
+void solve() {
+    ll n;
+    if (!(cin >> n)) return;
+    
+    ll x,y;
+    cin>>x>>y;
+    ll lc = lcm(x,y);
+    ll ans =0;
+    ll j =1, k =n;
+    for(ll i =x;i<=n;i=i+x){
+        if(i%lc != 0){
+            ans+= k; k--;
+        }
+    }
+
+    for(ll i =y;i<=n;i=i+y){
+        if(i%lc != 0){
+            ans-= j; j++;
+        }
+    }
+
+    print(ans);
+
+    // Solve logic here
+    
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
