ee362f0575a9146ee1c78116b9534da4350e9a90
Ved Choraria <vedchoraria19@gmail.com>
2026-03-18 07:56:29 +0530

    Time: 48 ms (72.31%) | Memory: 24.3 MB (91.38%) - LeetSync

--- LEETCODE/1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ LEETCODE/1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,33 +1,34 @@
+typedef pair<int, pair<int, int>> node;
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int rows = heights.size(), cols = heights[0].size();
-        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> minHeap;
-        minHeap.emplace(0, 0, 0);
+        priority_queue<node, std::vector<node>, std::greater<node>> pq;
+        int rows = heights.size() , cols = heights[0].size();
+        vector<vector<int>>dist(rows, vector<int> (cols, INT_MAX));
+        vector<pair<int,int>>dir = {
+            {1,0} , {-1,0} , {0 , 1}, {0,-1}
+        };
+
+        pq.push({0,{0,0}});
         dist[0][0] = 0;
-        
-        int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        while (!minHeap.empty()) {
-            auto [effort, x, y] = minHeap.top();
-            minHeap.pop();
-            
-            if (effort > dist[x][y]) continue;
-            
-            if (x == rows - 1 && y == cols - 1) return effort;
-            
-            for (auto& dir : directions) {
-                int nx = x + dir[0], ny = y + dir[1];
-                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
-                    int new_effort = max(effort, abs(heights[x][y] - heights[nx][ny]));
-                    if (new_effort < dist[nx][ny]) {
-                        dist[nx][ny] = new_effort;
-                        minHeap.emplace(new_effort, nx, ny);
+        while(!pq.empty()){
+            auto[effort , pos] = pq.top();
+            int r = pos.first , c= pos.second;
+            pq.pop();
+            if( r == rows -1 && c == cols-1)return effort;
+
+            for(auto[dr , dc] : dir){
+                int nr = r+dr , nc = c+dc;
+                if(nr>=0 && nr<rows && nc >=0 && nc < cols){
+                    int newEffort = max(effort , abs(heights[r][c] - heights[nr][nc]));
+                    if(newEffort < dist[nr][nc]){
+                        dist[nr][nc] = newEffort;
+                        pq.push({newEffort , {nr , nc}});
                     }
+                    
                 }
             }
         }
-        return -1;
+        return 0;
     }
 };
