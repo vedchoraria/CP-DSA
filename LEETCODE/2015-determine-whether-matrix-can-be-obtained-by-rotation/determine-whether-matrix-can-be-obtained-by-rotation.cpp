97bfb69a8545181a42ff4d48e2598b8ac3ac34fe
Ved Choraria <vedchoraria19@gmail.com>
2026-03-22 15:13:49 +0530

    Time: 2 ms (3.87%) | Memory: 14.5 MB (70.77%) - LeetSync

--- LEETCODE/2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ LEETCODE/2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,18 +1,26 @@
 class Solution {
+    void ratate(vector<vector<int>>& mat){
+        int n = mat.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                swap(mat[i][j], mat[n-1-j][n-1-i]);
+            }
+        }
+        for(int i=0; i<n/2; i++){
+            for(int j = 0; j<n; j++){
+                swap(mat[i][j], mat[n-1-i][j]);
+            }
+        }
+    }
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-        if(mat==target) return true;
-        int n=mat.size();
-        for(int r=0;r<4;r++){
-            if(mat==target) return true;
-            vector<vector<int>> temp(n,vector<int>(n));
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    temp[j][n-1-i]=mat[i][j];
-                }
-            }
-            mat=temp;
-        }
+        if(mat == target) return true;
+        ratate(mat);
+        if(mat == target) return true;
+        ratate(mat);
+        if(mat == target) return true;
+        ratate(mat);
+        if(mat == target) return true;
         return false;
     }
 };
