517cbefd1fca35d8db21fddd5174e0131a533066
Ved Choraria <vedchoraria19@gmail.com>
2026-04-04 11:54:29 +0530

    Time: 49 ms (12.71%) | Memory: 47.8 MB (27.97%) - LeetSync

--- LEETCODE/2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ LEETCODE/2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-    vector<string> split_sentence(string sen) {
-  
-    // Create a stringstream object
-    stringstream ss(sen);
     
-    // Variable to hold each word
-    string word;
-    
-    // Vector to store the words
-    vector<string> words;
-    
-    // Extract words from the sentence
-    while (ss >> word) {
       
-        // Add the word to the vector
-        words.push_back(word);
-    }
-    
-    return words;
-    }   
     string decodeCiphertext(string et, int rows) {
         int n = et.length();
-
         //BASE CASE : 
-        if(n<1) return et;
-        if(rows < 2) return et;
-        // int col =( ((n+3)/rows) + (n%rows) -1);
-        // int act = (n+3)/rows;
-        int i =0;int temp = 0;
-        int col = n/rows;
+        if(n<1 || rows < 2) return et;
+        int i =0 , temp = 0 , col = n/rows;
         vector<string>words;
         for(i;i<n;i= i+col){
             words.push_back(et.substr(i+temp,col));
             temp++;
-            cout<<i<<"   "<<temp<<endl;
-            
         }
-        // cout<<words[1]<<"jjkk"<<endl;
         string ans = "";
         for(int j = 0;j<words[0].size();j++){
             for(int i =0;i<rows;i++)
