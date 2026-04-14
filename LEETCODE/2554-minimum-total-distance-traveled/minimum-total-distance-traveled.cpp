e6b4b4b149aedf8fe653c6218f2174d20d2c51c0
Ved Choraria <vedchoraria19@gmail.com>
2026-04-14 20:22:42 +0530

    Time: 143 ms (19.18%) | Memory: 69.7 MB (19.18%) - LeetSync

---
+++ LEETCODE/2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+class Solution {
+public:
+ll solve(vector<int>&robot , vector<int>& pos , int i , int j , vector<vector<ll>>&dp){
+    if(i == robot.size()) return 0;
+    if(j == pos.size()) return 1e15;
+    if(dp[i][j]!= -1) return dp[i][j];
+    //take
+    ll take = 1LL*abs(robot[i]-pos[j]) + solve(robot , pos , i+1 , j+1 , dp);
+    ll notTake = solve(robot , pos , i ,j+1 ,dp);
+
+    return dp[i][j] = min(take , notTake);
+}
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin() , robot.end());
+        sort(factory.begin() , factory.end());
+        vector<int>pos;
+        for(auto it : factory){
+            int a = it[0] , b = it[1];
+            while(b--) pos.push_back(a);
+        }
+        int n = robot.size();
+        int m = pos.size();
+        vector<vector<ll>>dp(n, vector<ll>(m, -1));
+        return solve(robot , pos , 0 , 0 , dp);
+
+    }
+};
