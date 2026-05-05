31fd22e37e397f942780a872d563f43a389a5c78
Ved Choraria <vedchoraria19@gmail.com>
2026-05-06 00:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 79.3 MB (83.93%) - LeetSync

--- LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,21 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
+    int maxProfit(vector<int>& prices) {
+        int buy1 = INT_MIN, buy2 = INT_MIN;
+        int sell1 = 0, sell2 = 0;
 
-    int dp[100001][2][3];
+        for (int price : prices) {
+            buy1  = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
 
-    int solve(int i, int buy, int cap, vector<int>& prices) {
-
-        if(i == prices.size() || cap == 0)
-            return 0;
-
-        if(dp[i][buy][cap] != -1)
-            return dp[i][buy][cap];
-
-        int profit = 0;
-
-        if(buy) {
-
-            profit = max(
-                -prices[i] + solve(i+1, 0, cap, prices),
-                solve(i+1, 1, cap, prices)
-            );
-
-        } else {
-
-            profit = max(
-                prices[i] + solve(i+1, 1, cap-1, prices),
-                solve(i+1, 0, cap, prices)
-            );
+            buy2  = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
         }
 
-        return dp[i][buy][cap] = profit;
-    }
-
-    int maxProfit(vector<int>& prices) {
-
-        memset(dp, -1, sizeof(dp));
-
-        return solve(0, 1, 2, prices);
+        return sell2;
     }
 };
