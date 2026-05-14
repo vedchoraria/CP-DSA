a634446039e36d66c954b32e16afd4ec38faf84c
Ved Choraria <vedchoraria19@gmail.com>
2026-05-14 15:19:30 +0530

    Time: 1 ms (38.67%) | Memory: 16.3 MB (57.27%) - LeetSync

--- LEETCODE/64-minimum-path-sum/minimum-path-sum.cpp
+++ LEETCODE/64-minimum-path-sum/minimum-path-sum.cpp
@@ -12,11 +12,23 @@ public:
         int right = solve1(row, col+1, m , n, grid , dp) + grid[row][col];
         return dp[row][col]=min(down, right);
     }
+    int solve2(int row,int col, int m , int n, vector<vector<int>>&grid, vector<vector<int>>&dp){
+        if(row == 0 && col == 0){
+            return grid[row][col];
+        }
+        if(row <0 || col <0) return 1e9;
+
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int up = solve2(row-1, col , m , n, grid, dp) + grid[row][col];
+        int left = solve2(row, col-1, m , n, grid , dp) + grid[row][col];
+        return dp[row][col]=min(up, left);
+    }
     
     int minPathSum(vector<vector<int>>& grid) {
       int m = grid.size();
       int n = grid[0].size();
       vector<vector<int>>dp(m,vector<int>(n,-1));
-      return solve1(0,0,m,n,grid,dp);  
+      return solve2(m-1,n-1,m,n,grid,dp);  
     }
 };
