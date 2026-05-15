a78655541104acdc7d08e9e246f650cc669d43bc
Ved Choraria <vedchoraria19@gmail.com>
2026-05-15 15:07:56 +0530

    Time: 83 ms (76.32%) | Memory: 13.5 MB (83.63%) - LeetSync

--- LEETCODE/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ LEETCODE/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-bool solve(vector<vector<int>>&dp, int i, int sum ,int tot, vector<int>&nums){
-    if(tot == (sum)) return true;
-    if(i == nums.size()) return false;
-    if(dp[i][sum] != -1) return dp[i][sum];
 
-    int nt = solve(dp,i+1,sum,tot,nums);
-    int t = false;
-    if(tot>=(sum))
-    t= solve(dp,i+1,sum+nums[i], tot, nums);
-    return dp[i][sum] = (t||nt);
-}
     bool canPartition(vector<int>& nums) {
-        int total_Sum=0;
-        int n = nums.size();
 
-        for(int i : nums) total_Sum += i;
-        if(total_Sum&1) return false;
-        vector<vector<int>>dp(n,vector<int>(total_Sum+1,-1));
-        return solve(dp,0,0,total_Sum/2, nums);
+        int sum = 0;
+
+        for(int x : nums)
+            sum += x;
+
+        // odd sum impossible
+        if(sum % 2)
+            return false;
+
+        int target = sum / 2;
+
+        vector<bool> dp(target + 1, false);
+
+        dp[0] = true;
+
+        for(int num : nums){
+
+            for(int s = target; s >= num; s--){
+
+                dp[s] = dp[s] || dp[s - num];
+            }
+        }
+
+        return dp[target];
     }
 };
