8c189ed60f73c362f49e081d3c9236191f5a0ff4
Ved Choraria <vedchoraria19@gmail.com>
2026-05-16 13:58:21 +0530

    Time: 15 ms (16.14%) | Memory: 44.8 MB (77.07%) - LeetSync

---
+++ LEETCODE/455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(),s.end());
+        int i = g.size()-1, j= s.size()-1;
+        int cnt =0;
+        while(i>=0 && j>=0){
+            if(g[i] <= s[j]){
+                i--; j--; cnt++;
+            }
+            else i--;
+        }
+        return cnt;
+    }
+};
