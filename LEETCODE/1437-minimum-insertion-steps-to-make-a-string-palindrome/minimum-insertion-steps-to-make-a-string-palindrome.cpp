c0bb1cfb7f116725b0f7a4ece486a95b51ea7b09
Ved Choraria <vedchoraria19@gmail.com>
2026-05-23 16:17:46 +0530

    Time: 19 ms (88.51%) | Memory: 9.7 MB (90.94%) - LeetSync

--- LEETCODE/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ LEETCODE/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-int solve(int i,int j,string &a, string &b, vector<vector<int>> &dp){
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(a[i]==b[j]){
-            return dp[i][j]=1+solve(i-1,j-1,a,b,dp);
+
+    int lcs(string &s1, string &s2){
+
+        int n=s1.size();
+
+        vector<int>curr(n+1, 0);
+        vector<int>prev(n+1, 0);
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1]==s2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }
+                else{
+                    curr[j]=max(prev[j], curr[j-1]);
+                }
+            }
+            prev=curr;
         }
-        return dp[i][j]=max(solve(i-1,j,a,b,dp),solve(i,j-1,a,b,dp));
+
+        return prev[n];
+
     }
+
     int minInsertions(string s) {
-        int n = s.size();
-        string a = s;
-        reverse(a.begin(), a.end());
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return n- solve(n-1,n-1,s,a,dp);
-        // for (int i = 0; i <= n; i++) {
-        //     dp[i][0] = 0;
-        //     dp[0][i] = 0;
-        // }
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= n; j++) {
-        //         // match
-        //         if (s[i - 1] == a[j - 1]) {
-        //             dp[i][j] = 1 + dp[i - 1][j - 1];
-        //         } else {
-        //             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-        //         }
-        //     }
-        // }
-        // return n - dp[n][n];
+
+        int n =s.size();
+        string t=s;
+        reverse(t.begin(), t.end());
+
+        return n-lcs(s, t);
     }
 };
