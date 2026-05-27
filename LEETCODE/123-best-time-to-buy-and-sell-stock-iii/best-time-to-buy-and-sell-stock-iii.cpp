adfd2d45c5dcf14556941925d62f4955224f7b79
Ved Choraria <vedchoraria19@gmail.com>
2026-05-27 10:48:51 +0530

    Time: 464 ms (14.18%) | Memory: 223.9 MB (20.33%) - LeetSync

--- LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,54 @@
-#include <vector>
-#include <climits>
-using namespace std;
+// class Solution {
+// public:
+// int solve(int index, int buy, int cap, vector<int>& prices){
+//     int n = prices.size();
+//     if(index == n) return 0;
+
+//     if(cap == 0) return 0;
+//     int profit =0;
+//     if(buy){
+//         int buyKaro = -prices[index] + solve(index+1, 0, cap, prices);
+//         int skipKaro = 0 + solve(index+1, 1, cap, prices);
+//         profit = max(buyKaro , skipKaro);
+//     }
+//     else{
+//         int sellKaro = prices[index] + solve(index+1, 1, cap-1 , prices);
+//         int skipKaro = 0 + solve(index+1, 0, cap, prices);
+//         profit = max(sellKaro , skipKaro);
+//     }
+//     return profit;
+// }
+//     int maxProfit(vector<int>& prices) {
+//         return solve(0, 1, 2, prices);
+//     }
+// };
 
 class Solution {
 public:
+int solve(int index, int buy, int cap, vector<int>& prices , vector<vector<vector< int>>> &dp){
+    int n = prices.size();
+    if(index == n) return 0;
+
+    if(cap == 0) return 0;
+
+    if(dp[index][buy][cap] != -1) return dp[index][buy][cap];
+    int profit =0;
+    if(buy){
+        int buyKaro = -prices[index] + solve(index+1, 0, cap, prices, dp);
+        int skipKaro = 0 + solve(index+1, 1, cap, prices, dp);
+        profit = max(buyKaro , skipKaro);
+    }
+    else{
+        int sellKaro = prices[index] + solve(index+1, 1, cap-1 , prices, dp);
+        int skipKaro = 0 + solve(index+1, 0, cap, prices, dp);
+        profit = max(sellKaro , skipKaro);
+    }
+    return dp[index][buy][cap] = profit;
+}
     int maxProfit(vector<int>& prices) {
-        int buy1 = INT_MIN, buy2 = INT_MIN;
-        int sell1 = 0, sell2 = 0;
-
-        for (int price : prices) {
-            buy1  = max(buy1, -price);
-            sell1 = max(sell1, buy1 + price);
-
-            buy2  = max(buy2, sell1 - price);
-            sell2 = max(sell2, buy2 + price);
-        }
-
-        return sell2;
+        //index, buy, cap
+        int n = prices.size();
+        vector<vector<vector< int>>> dp(n,vector<vector<int>> ( 2 , vector<int> (3,-1) ) );
+        return solve(0, 1, 2, prices,dp);
     }
 };
