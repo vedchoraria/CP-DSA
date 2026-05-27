e5acb78ca0648ca9d9a349ba87a72d0310aad637
Ved Choraria <vedchoraria19@gmail.com>
2026-05-27 11:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 79.1 MB (93.93%) - LeetSync

--- LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ LEETCODE/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -86,36 +86,58 @@
 // };
 
 
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         // index, buy, cap
+
+// int n = prices.size();
+//         vector<vector<int>> prev (2, vector<int>(3, 0));
+//         vector<vector<int>> curr (2, vector<int>(3, 0));
+
+//         for (int index = n - 1; index >= 0; index--) {
+//             for (int buy = 0; buy <= 1; buy++) {
+//                 for (int cap = 2; cap > 0; cap--) {
+//                     int profit = 0;
+//                     if (buy) {
+//                         int buyKaro = -prices[index] + prev[0][cap];
+                                      
+//                         int skipKaro = 0 + prev[1][cap]; 
+//                         profit = max(buyKaro, skipKaro);
+//                     } else {
+//                         int sellKaro = prices[index] + prev[1][cap-1];
+//                         int skipKaro = 0 + prev[0][cap]; 
+//                         profit = max(sellKaro, skipKaro);
+//                     }
+//                     curr[buy][cap] = profit;
+//                 }
+//             }
+
+//             prev = curr;
+//         }
+
+//         return curr[1][2];
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // index, buy, cap
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
 
-int n = prices.size();
-        vector<vector<int>> prev (2, vector<int>(3, 0));
-        vector<vector<int>> curr (2, vector<int>(3, 0));
+        for (int i = 1; i < prices.size(); i++) {
+            int price = prices[i];
 
-        for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int cap = 2; cap > 0; cap--) {
-                    int profit = 0;
-                    if (buy) {
-                        int buyKaro = -prices[index] + prev[0][cap];
-                                      
-                        int skipKaro = 0 + prev[1][cap]; 
-                        profit = max(buyKaro, skipKaro);
-                    } else {
-                        int sellKaro = prices[index] + prev[1][cap-1];
-                        int skipKaro = 0 + prev[0][cap]; 
-                        profit = max(sellKaro, skipKaro);
-                    }
-                    curr[buy][cap] = profit;
-                }
-            }
+            buy1 = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
 
-            prev = curr;
+            buy2 = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
         }
 
-        return curr[1][2];
+        return sell2;
     }
 };
