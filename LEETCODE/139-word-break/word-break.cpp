8fef7bbc605bd033ad24bf48803963cd6fc016b7
Ved Choraria <vedchoraria19@gmail.com>
2026-05-28 15:55:00 +0530

    Time: 3 ms (72.91%) | Memory: 13.4 MB (60.28%) - LeetSync

--- LEETCODE/139-word-break/word-break.cpp
+++ LEETCODE/139-word-break/word-break.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-//     bool solve(int i, unordered_map<string,int>&mpp, string &s, string temp, vector<int>&dp){
-//         if(i < 0){
-//     return mpp[temp];
-// }
-//     if(dp[i] != -1) return dp[i];
-//         temp = s[i] + temp;
-//         bool reduce = false;
-//         if(mpp[temp])
-//          reduce = solve(i-1, mpp,s,"", dp);
+int dp[300];
+int help(int i , string s, set<string>& dic){
+    if(i == s.size()) return 1;
+    string temp;
+    if(dp[i] != -1) return dp[i];
 
-//         bool nreduce = solve(i-1,mpp,s,temp,dp);
-
-//         return dp[i] = reduce||nreduce;
-
-//     }
-    bool wordBreak(string s, vector<string>& dic) {
-        unordered_map<string,int>mpp;
-        for(string i : dic) mpp[i] = 1;
-        int n = s.size();
-        vector<int>dp(n+1,0);
-        // return solve(n-1, mpp,s,"", dp);
-        dp[n] = true;
-        for(int i = n-1;i>=0;i--){
-            string temp = "";
-            for(int j = i;j<n;j++){
-                temp += s[j];
-                if(mpp[temp] && dp[j+1])
-                {
-                    dp[i] = true;
-                    break;
-                }
-            }
+    dp[i] = 0;
+    for(int j = i;j<s.size();j++){
+        temp += s[j];
+        if(dic.count(temp)){
+            // found a word from j+1
+            dp[i] = help(j+1,s,dic);
+            if(dp[i]) return dp[i];
         }
-        return dp[0];
+    }
+
+    return dp[i] = 0;
+}
+    bool wordBreak(string s, vector<string>& dic) {
+        set<string>st ;
+        for(auto a : dic) st.insert(a);
+        memset(dp,-1,sizeof dp);
+        return help(0,s,st);
     }
 };
