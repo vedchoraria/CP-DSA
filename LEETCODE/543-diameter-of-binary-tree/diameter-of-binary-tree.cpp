3b205d5acd23a43b05b31c7673f34f7fa5cb4bbd
Ved Choraria <vedchoraria19@gmail.com>
2026-06-08 22:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (71.66%) - LeetSync

---
+++ LEETCODE/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int diameter = 0;
+
+    int height(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return diameter;
+    }
+};
