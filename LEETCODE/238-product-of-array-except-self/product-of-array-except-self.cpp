1270bfe32cf4e56161dd933f65dbc1a58da2eaab
Ved Choraria <vedchoraria19@gmail.com>
2026-06-13 20:44:01 +0530

    Time: 6 ms (6.87%) | Memory: 43.7 MB (6.02%) - LeetSync

--- LEETCODE/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ LEETCODE/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,23 @@
-
-#define ll long long 
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        ll n = nums.size();
-        // This will not work because element can be 0, and 0/0 = not defined, see tc 2
-        // for(ll i : nums) total *= i;
-        // for(ll &i : nums) i = total/i;
+        int n=nums.size();
+        vector<int>pre(n,1);
+        vector<int>suff(n,1);
+        vector<int>re;
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=pre[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i]=suff[i+1]*nums[i+1];
 
-        vector<ll>pref(n,1);
-        vector<ll>suff(n,1);
-        for(ll i =1;i<n;i++){
-            pref[i] = pref[i-1]*nums[i-1];
         }
-        for(ll i = n-2; i>=0;i--){
-            suff[i] = suff[i+1]*nums[i+1];
+        for(int i=0;i<n;i++)
+        {
+            re.push_back(suff[i]*pre[i]);
         }
-        vector<int>ans(n,1);
-        for(ll i =0;i<n;i++){
-            ans[i] = (int)pref[i]*suff[i];
-        }
-
-        return ans;
+        return re;
     }
 };
