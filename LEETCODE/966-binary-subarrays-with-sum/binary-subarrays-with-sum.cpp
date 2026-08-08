165327faf67dbbae3cf274c3b6b73df33265deed
Ved Choraria <vedchoraria19@gmail.com>
2026-08-08 14:29:36 +0530

    Time: 2 ms (57.75%) | Memory: 32.7 MB (63.54%) - LeetSync

--- LEETCODE/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ LEETCODE/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int>freq;
-        int n = nums.size();
-        int sum =0;
+
+    int atMost(vector<int>& nums, int k) {
+        if (k < 0)
+            return 0;
+
+        int left = 0;
+        int sum = 0;
         int ans = 0;
-        freq[0] = 1;
-        for(int right = 0;right < n; right++){
+
+        for (int right = 0; right < nums.size(); right++) {
+
             sum += nums[right];
-            
-            int need = sum - goal;
-            ans += freq[need];
-            freq[sum]++;
+
+            while (sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            // Number of valid subarrays ending at right
+            ans += right - left + 1;
         }
+
         return ans;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 };
