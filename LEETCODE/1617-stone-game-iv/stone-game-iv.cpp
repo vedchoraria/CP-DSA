1897619f20621471b37f0d28159c52d4e623fe87
Ved Choraria <vedchoraria19@gmail.com>
2026-08-10 07:29:00 +0530

    Time: 31 ms (81.44%) | Memory: 9.1 MB (73.64%) - LeetSync

--- LEETCODE/1617-stone-game-iv/stone-game-iv.cpp
+++ LEETCODE/1617-stone-game-iv/stone-game-iv.cpp
@@ -17,7 +17,6 @@ public:
 
         vector<bool>dp(n+1,false);
         for(int i = 1; i <= n; i++){
-            
             for(int j =1 ; j*j <= i;j++){
                 if(!dp[i-j*j]) {dp[i] = true; break;}
             }
