c77462b52a77f54f3a139f6d3d7fb7cc4a6c0572
Ved Choraria <vedchoraria19@gmail.com>
2026-08-25 15:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.40%) - LeetSync

---
+++ LEETCODE/20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '[') st.push(c);
+            else{
+                if(st.empty()) return false;
+                if(st.top() == '('){
+                    if(c != ')') return false;
+                    st.pop();
+                }
+                else if(st.top() == '{') {if(c != '}') return false; st.pop();}
+                else{
+                    if(c != ']') return false;
+                    st.pop();
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+};
