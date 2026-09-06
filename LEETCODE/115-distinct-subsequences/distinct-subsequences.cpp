a27908d4eb408089189b90485af71d64c2ea7e23
Ved Choraria <vedchoraria19@gmail.com>
2026-09-06 21:37:36 +0530

    Time: 38 ms (23.53%) | Memory: 27 MB (52.57%) - LeetSync

--- LEETCODE/115-distinct-subsequences/distinct-subsequences.cpp
+++ LEETCODE/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,21 @@
 class Solution {
-public:
-    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        int n = s.size(), m = t.size();
-        if(j == m) return 1;
-        else if(i== n || j==m) return 0;
-
+    int recurse(int i, int j, string &s, string &t, vector<vector<int>>&dp){
+        if(j== -1) return 1;
+        if(i<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int ans =0;
-        if(s[i] == t[j]) {
-            ans += solve(i+1, j+1, s, t, dp);
+        int cnt = 0;
+        if(s[i] == t[j]){
+            cnt += recurse(i-1, j-1, s, t, dp);
+            cnt += recurse(i-1, j, s, t, dp);
+        }else{
+            cnt += recurse(i-1, j, s, t, dp);
         }
-        ans+= solve(i+1, j, s,t,dp);
-
-        return dp[i][j] = ans;
+        return dp[i][j] = cnt;
     }
+public:
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return solve(0,0,s,t,dp);
-
+        int m = s.size(), n = t.size();
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return recurse(m-1, n-1, s, t, dp);
     }
 };
