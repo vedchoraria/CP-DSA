19bee5f79b26bec13375f7761a6311062fb5f62b
Ved Choraria <vedchoraria19@gmail.com>
2026-09-23 22:00:32 +0530

    Time: 3 ms (71.49%) | Memory: 102.5 MB (38.48%) - LeetSync

--- LEETCODE/1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ LEETCODE/1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,39 +1,20 @@
 class Solution {
-    using ll = long long;
 public:
-    int minOperations(vector<int>& v, int xx) {
-        int mn = 1e7, n = v.size();
-        ll s = 0, x = xx;
-        int l = 0;
-        for(int i=0; i<n; i++){
-            if(s + v[i] == x){
-                s = x;
-                l = i;
-                break;
-            }else if(s > x) break;
-            s += v[i];
-            l = i;
+    int minOperations(vector<int>& A, int x) {
+        int k = reduce(A.begin(), A.end()) - x, n = A.size();
+        if (k < 0) return -1;
+        if (k == 0) return n;
+
+        int best = -1, i = 0, sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += A[j];
+            while (sum > k)
+                sum -= A[i++];
+
+            if (sum == k)
+                best = max(best, j - i + 1);
         }
-        if(s < x) return -1;
-        int r = n;
-        while(l >= -1 && r > 0){
-            if(s == x){
-                mn = min(mn, l+1+n-r);
-                if(l>=0){
-                    s -= v[l];
-                    l--;
-                }else break;
-            }else if(s < x){
-                r--;
-                s += v[r];
-            }else{
-                if(l>=0){
-                    s -= v[l];
-                    l--;
-                }else break;
-            }
-        }
-        if(mn == 1e7) mn = -1;
-        return mn;
+
+        return best + 1 ? n - best : -1;
     }
 };
